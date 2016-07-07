017c411df6dbceca48fbb3931cc267d2326bf072
lifecrazy <hengzhang0630@126.com>
2016-07-06 22:34:51 -0700

    modify the leetcode001.cpp

--- leetcode001.cpp
+++ leetcode001.cpp
@@ -7,15 +7,18 @@
 
 #include<iostream>
 #include<vector>
+#include<map>
 using namespace std;
 
+//方法一：
 //思路：依次遍历每个元素和其后的每个的元素的和，判断其是否等于target
+//时间复杂度为：n2
 vector<int> twoSum(vector<int>& nums, int target) {
     vector<int> results;
     size_t len = nums.size();
-    for (int i = 0; i < len ; i++)
+    for (size_t i = 0; i < len ; i++)
     {
-        for(int j = i + 1; j < len ; j++)
+        for(size_t j = i + 1; j < len ; j++)
         {
             if(target == (nums[i] + nums[j]))
             {
@@ -27,7 +30,29 @@ vector<int> twoSum(vector<int>& nums, int target) {
     }
     return results;
 }
-
+//方法二：
+//思路：将数组中的所有数字存储在map 或者set中key 为 数组的index,value为数组的值
+//然后依次查找 其时间复杂度为o(n) 该方法是用空间换时间
+vector<int> twoSum_2(vector<int>& nums, int target) {
+    vector<int> results;
+    map<int,int> mapData;
+    size_t len = nums.size();
+    for(size_t i = 0; i < len ; i++)
+    {
+        mapData.insert(pair<int,int>(i,nums[i]));
+    }
+    for(size_t i = 0;i < len ; i++)
+    {
+        int temp = target - nums[1];
+        if(mapData.find(temp)!= mapData.end())
+        {
+            results.push_back(i);
+            results.push_back(mapData.find(temp)->first);
+            return results;
+        }
+    }
+    return results;
+}
 int main()
 {
     vector<int> inputs;
@@ -36,10 +61,18 @@ int main()
     inputs.push_back(11);
     inputs.push_back(15);
     int target = 9;
+    cout << "twoSum results" << endl;
     vector<int> results  = twoSum(inputs,target);
-    for(int i = 0 ; i < results.size();i++)
+    for(size_t i = 0 ; i < results.size();i++)
     {
         cout << results[i] << "\t";
     }
-    cout <<endl;
+    cout << endl;
+    cout << "twoSum_2 results_2" << endl;
+    vector<int> results_2 = twoSum_2(inputs,target);
+    for(size_t i = 0 ; i < results.size() ; i++)
+    {
+        cout << results_2[i] << "\t";
+    }
+    cout << endl;
 }
