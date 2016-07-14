01f8fc4a6ce211b99ad2c4059b8205291c7c055f
lifecrazy <hengzhang0630@126.com>
2016-07-13 23:09:02 -0700

    modify the leetcode125.cpp

--- leetcode125.cpp
+++ leetcode125.cpp
@@ -14,23 +14,26 @@ using namespace std;
      int j = s.length() -1;
      while(i <= j)
      {
-         if(!((s[i] >= 'a' && s[i] <='z') || ( s[i] >= 'A' && s[i] <= 'Z')))
+         if(!((s[i] >= '0' && s[i] <= '9') || (s[i] >= 'a' && s[i] <='z') || ( s[i] >= 'A' && s[i] <= 'Z')))
          {
              i++;
              continue;
          }
-         if(!((s[j] >= 'a' && s[j] <='z') || ( s[j] >= 'A' && s[j] <= 'Z')))
+         if(!((s[j] >= '0' && s[j] <= '9') || (s[j] >= 'a' && s[j] <='z') || ( s[j] >= 'A' && s[j] <= 'Z')))
          {
              j--;
              continue;
          }
-         if(!(s[i] == s[j]|| s[i] == (s[j] + 'a' - 'A')||s[i] == (s[j] + 'A' - 'a')))
+         if( toupper(s[i]) == toupper(s[j]))
+         {
+             i++;
+             j--;
+         }
+         else
          {
              flag = false;
              break;
          }
-         i++;
-         j--;
      }
      return flag;
  }
@@ -38,9 +41,9 @@ using namespace std;
  int main()
  {
      string s1 = "A man, a plan, a canal: Panama";
-     string s2 = "a";
-     string s3 = "";
-     cout <<"resutl of s1:"<< isPalindrome(s1)<<endl;
-     cout <<"s2:"<< isPalindrome(s2)<<endl;
+     string s2 = "abccba";
+     string s3 = "0P";
+    cout <<"resutl of s1:"<< isPalindrome(s1)<<endl;
+    cout <<"s2:"<< isPalindrome(s2)<<endl;
      cout <<"s3:"<< isPalindrome(s3)<<endl;
  }
