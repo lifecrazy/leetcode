654f0f6f8f0391c1feb78eead1f0f1026221f636
lifecrazy <hengzhang0630@126.com>
2016-08-08 23:12:24 -0700

    palindrome linked list

---
+++ leetcode234.cpp
@@ -0,0 +1,115 @@
+/*************************************************************************
+> File Name: leetcode243.cpp
+> Author: 
+> Mail: 
+> Created Time: Sun 07 Aug 2016 06:16:13 PM PDT
+************************************************************************/
+
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+//思路：1.将链表分为前后两半部分
+//2.将后半部分链表逆置
+//3.比较两部分是否相同
+void divideList(ListNode* head, ListNode* &front, ListNode* &end)
+{
+    ListNode* first = head;
+    ListNode* sec = head;
+    ListNode* pPre = head;
+    while(first != NULL)
+    {
+        pPre = sec;
+        first = first->next;
+        if(first != NULL)
+        {
+            first = first->next;
+        }
+        sec = sec->next;
+    }
+    front = head;
+    end = pPre->next;
+    pPre->next = NULL;
+}
+void reverseList(ListNode* &head)
+{
+    if(head == NULL || head->next == NULL)
+    {
+        return ;
+    }
+    ListNode* pCur = head;
+    ListNode* pPre = NULL;
+    while(pCur != NULL)
+    {
+        ListNode* pNext = pCur->next;
+        pCur->next = pPre;
+        pPre = pCur;
+        if(pNext == NULL)
+        {
+            break;
+        }
+        pCur = pNext;
+    }
+    head = pCur;
+}
+bool isPalindrome(ListNode* head) {
+    if(head == NULL || head->next == NULL)
+    {
+        return true;
+    }
+    ListNode* front;
+    ListNode* end;
+    divideList(head,front,end);
+    reverseList(end);
+    while(front != NULL && end != NULL)
+    {
+        if(front->val != end->val)
+        {
+            return false;
+        }
+        front = front->next;
+        end = end->next;
+    }
+    return true;
+}
+
+int main()
+{
+    ListNode list(-1);
+    int num = 6;
+    ListNode* temp = &list;
+    for(int i = 0 ; i < num; i++)
+    {
+        ListNode* node = new ListNode(i);
+        temp->next = node;
+        temp = temp->next;
+    }
+    ListNode listsec(-1);
+    ListNode *pCur = &listsec;
+    for(int i = num ;i > 0; i--)
+    {
+        ListNode* node = new ListNode(i);
+        pCur->next = node;
+        pCur= pCur->next;
+    }
+    temp->next = listsec.next;
+    ListNode* p = list.next;
+    while(p!= NULL)
+    {
+        cout << p->val << "\t";
+        p = p->next;
+    }
+    cout << endl;
+    cout << isPalindrome(list.next) << endl;
+    p = list.next;
+    while(p!= NULL)
+    {
+        cout << p->val << "\t";
+        p = p->next;
+    }
+    cout << endl;
+}
+
