fb8d45718192b8776a797b0442a4bc34fadd71ad
lifecrazy <hengzhang0630@126.com>
2016-08-09 18:40:50 -0700

    odd even linked list

---
+++ leetcode328.cpp
@@ -0,0 +1,70 @@
+/*************************************************************************
+	> File Name: leetcode328.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Tue 09 Aug 2016 06:20:26 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ListNode* oddEvenList(ListNode* head) {
+    if(head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+    ListNode oddNode(-1);
+    ListNode evenNode(-1);
+    ListNode* pOdd = &oddNode;
+    ListNode* pEven = &evenNode;
+    ListNode* pCur = head;
+    int i = 0;
+    while(pCur != NULL)
+    {
+        if(i % 2 == 0)
+        {
+            pOdd->next = pCur;
+            pOdd = pOdd->next;
+        }
+        else
+        {
+            pEven->next = pCur;
+            pEven = pEven->next;
+        }
+        pCur = pCur->next;
+        i++;
+    }
+    pOdd->next = evenNode.next;
+    return oddNode.next;
+}
+int main()
+{
+    ListNode data(-1);
+    ListNode* pCur = &data;
+    int num = 10 ;
+    for(int i = 0;i < num; i++)
+    {
+        ListNode* node = new ListNode(i);
+        pCur->next = node;
+        pCur = pCur->next;
+    }
+    pCur = data.next;
+    while(pCur != NULL)
+    {
+        cout << pCur->val << "\t";
+        pCur = pCur->next;
+    }
+    cout << endl;
+    pCur = oddEvenList(data.next);  
+    while(pCur != NULL)
+    {
+        cout << pCur->val << "\t";
+        pCur = pCur->next;
+    }
+    cout << endl;
+}
