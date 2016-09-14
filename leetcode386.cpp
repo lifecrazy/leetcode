bbd095b592082426477ff16b79c9b716fdb87673
lifecrazy <hengzhang0630@126.com>
2016-09-14 00:32:29 -0700

    lexicographical number

--- leetcode386.cpp
+++ leetcode386.cpp
@@ -1,47 +1,28 @@
 /*************************************************************************
-	> File Name: leetcode386.cpp
-	> Author: 
-	> Mail: 
-	> Created Time: Tue 13 Sep 2016 11:28:15 PM PDT
- ************************************************************************/
+> File Name: leetcode386.cpp
+> Author: 
+> Mail: 
+> Created Time: Tue 13 Sep 2016 11:28:15 PM PDT
+************************************************************************/
 
 #include<iostream>
 #include<vector>
 using namespace std;
+vector<int> ans;
+void dfs(int cur, int n)
+{
+    if(cur > n)
+        return;
+    ans.push_back(cur);
+    for(int i=0; i<=9; i++)
+        if(cur * 10 + i <= n)
+            dfs(cur * 10 + i, n);
+
+}
 vector<int> lexicalOrder(int n) {
-    vector<int> result;
-    int base = 10;
-    if(n <= 9)
-    {
-        for(int i = 0 ; i < n ;i++)
-        {
-            result.push_back(i + 1);
-        }
-        return result;
-    }
-    for(int i = 1;i <= 9 ; i++)
-    {
-        result.push_back(i);
-        if(n >= i * 10)
-        {
-            while(base <= n)
-            {
-                if(n > (i+1)* base)
-                {
-                    for(int j = base * i;j < (i+1)*base;j++)
-                        result.push_back(j);    
-                }
-                else
-                {
-                    for(int j = base * i ;j <= n;j++)
-                        result.push_back(j);
-                }
-                base = base * 10;
-            }
-            base = 10;
-        }
-    }
-    return result;
+    for(int i=1; i<=9; i++)
+        dfs(i, n);
+    return ans;
 }
 
 int main()
