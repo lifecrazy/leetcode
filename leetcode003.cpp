374cfc91e5a6a0eb51f09f3248a43d33086091d3
lifecrazy <hengzhang0630@126.com>
2016-09-20 19:10:00 -0700

    Longest Substring Without Repeating Characters

--- leetcode003.cpp
+++ leetcode003.cpp
@@ -12,6 +12,11 @@ using namespace std;
 bbbbb      b    1.
 pwwkew     wke  3.
 * */
+//思路：让当前元素从第一个元素开始遍历并计数cur，如果发现
+//有相同的元素则cur回1，并下次的起始点从这个相同的元素的
+//下一个开始才重新计数
+//当前元素计数完毕之后更新最大值，如果当前的不同元素的数量
+//比之前的最大值大则更新当前值
 int lengthOfLongestSubstring(string s) {
     int len = 0 ;
     int start = 0;
