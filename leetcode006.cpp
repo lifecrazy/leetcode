732dc87cf1845f813ca83b0c5cf4526e45d7fd4c
lifecrazy <hengzhang0630@126.com>
2016-09-21 00:55:30 -0700

    zigzag conversion

--- leetcode006.cpp
+++ leetcode006.cpp
@@ -1,28 +1,33 @@
 /*************************************************************************
-	> File Name: leetcode006.cpp
-	> Author: 
-	> Mail: 
-	> Created Time: Wed 21 Sep 2016 12:12:08 AM PDT
- ************************************************************************/
+> File Name: leetcode006.cpp
+> Author: 
+> Mail: 
+> Created Time: Wed 21 Sep 2016 12:12:08 AM PDT
+************************************************************************/
 
 #include<iostream>
 using namespace std;
 
 string convert(string s, int numRows) {
+    if(numRows == 1 || s.length() <= numRows)
+    return s;
     string result = "";
     for(int i = 0 ; i < numRows;i++)
     {
         int j = i;
         int step = numRows + numRows - 2;
         int k = step - i;
-        while(j < s.length() && k < s.length()+step)
+        while(j < s.length() )
         {
             result +=s[j];
             j += step;
             if(!(i == 0 || i == numRows - 1))
             {
-                result +=s[k];
-                k += step;
+                if(k < s.length())
+                {
+                    result +=s[k];
+                    k += step;
+                }
             }
         }
     }
@@ -31,5 +36,7 @@ string convert(string s, int numRows) {
 int main()
 {
     cout << convert("PAYPALISHIRING", 3) <<endl;
+    cout << convert("P", 1) <<endl;
     cout << convert("abcdefghijkl", 4) <<endl;
+    cout << convert("abcde", 4) <<endl;
 }
