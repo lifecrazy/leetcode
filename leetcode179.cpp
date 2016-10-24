e1204f8324aa027ed5596c2c34b0f37da7ba8d23
heng <hengzhang0630@126.com>
2016-10-23 22:52:53 -0700

    largest number

---
+++ leetcode179.cpp
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: leetcode179.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 23 Oct 2016 10:37:22 PM PDT
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+bool cmp(string a,string b)
+{
+    return a + b > b + a;
+}
+string largestNumber(vector<int>& nums) {
+    vector<string> s_num;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        s_num.push_back(to_string(nums[i]));
+    }
+    sort(s_num.begin(),s_num.end(),cmp);
+    string res = "";
+    for(int i = 0; i <s_num.size();i++)
+    {
+        res += s_num[i];
+    }
+    if(res == "0")
+        return "0";
+    return res;
+}
+int main()
+{
+    vector<int> data;
+    data.push_back(3);
+    data.push_back(30);
+    data.push_back(34);
+    data.push_back(5);
+    data.push_back(9);
+    cout << largestNumber(data) << endl;
+}
