c0bdb1e25bbf3507d89c9954d35d52a9376d5f5a
heng <hengzhang0630@126.com>
2016-10-24 23:46:52 -0700

    h-index

--- leetcode274.cpp
+++ leetcode274.cpp
@@ -16,27 +16,20 @@ int hIndex(vector<int>& citations) {
     {
         return 0;
     }
-    int index = 0;
     sort(citations.begin(),citations.end());
-    for(int i = citations.size() -1 ; i >= 0;i--)
+    for(int i = citations.size() -1 ; i >= 0 && citations[i] >= (citations.size() - i);i--)
     {
-       // cout << citations[i] << "\t";
-        if(citations[i] < index)
-        {
-            break;
-        }
-        index++;
-        result = citations[i];
+        result = citations.size() - i;
     }
     return result;
 }
 int main()
 {
     vector<int> nums;
-    nums.push_back(3);
-    nums.push_back(0);
-    nums.push_back(6);
+   // nums.push_back(3);
+   // nums.push_back(0);
+   // nums.push_back(6);
     nums.push_back(1);
-    nums.push_back(5);
+   // nums.push_back(5);
     cout << hIndex(nums) << endl;
 }
