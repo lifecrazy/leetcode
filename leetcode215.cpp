f46a4592f1e60e36d3cce7cfa7dabd9fdf4c0c5b
heng <hengzhang0630@126.com>
2016-10-26 19:19:05 -0700

    kth largest element in an array

--- leetcode215.cpp
+++ leetcode215.cpp
@@ -8,7 +8,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-
+/* this two funciton has bug
 int QuickSort(vector<int>& nums,int left,int right)
 {
     int p = nums[left];
@@ -50,13 +50,49 @@ int QuickSort_KMax(vector<int>& nums,int left,int right,int k)
             return nums[mid];
         }
     }
-}
+}*/
 int findKthLargest(vector<int>& nums, int k) 
 {
-    return QuickSort_KMax(nums,0,nums.size() - 1 ,nums.size() - k);
+    int left = 0;
+    int right = nums.size() - 1;
+    while(left < right)
+    {
+        int key = nums[left];
+        int l = left;
+        int r = right;
+        while(l < r)
+        {
+            while(l < r && nums[r] < key)
+            {
+                --r;
+            }
+            nums[l] = nums[r];
+            while(l < r && nums[l] >= key) 
+            {
+                ++l;
+            }
+            nums[r] = nums[l];
+        }
+        nums[l] = key;
+        if(l == k -1)
+        {
+            return nums[k - 1];
+        }
+        else if(l > k - 1)
+        {
+            right = l -1;
+        }
+        else
+        {
+            left = l + 1;
+        }
+    }
+    return nums[k -1];
 }
 int main()
 {
+    //[3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6]
+    //2
     vector<int> nums;
     nums.push_back(10);
     nums.push_back(7);
