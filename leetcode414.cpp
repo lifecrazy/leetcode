dc587c89fbe66da78ffbfb6054585753c17b22c1
heng <hengzhang0630@126.com>
2016-10-27 23:05:54 -0700

    Third Maximum Number

--- leetcode414.cpp
+++ leetcode414.cpp
@@ -8,9 +8,80 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<set>
 using namespace std;
+//方法一利用set自动排序的功能 当set大小超过3的时候直接删除第一个元素即可
+//始终保持第一个元素为第三大元素
+int thirdMax(vector<int>& nums)
+{
+    set<int> s;
+    for(int i = 0; i < nums.size();i++)
+    {
+        s.insert(nums[i]);
+        if(s.size() > 3)
+        {
+            s.erase(s.begin());
+        }
+    }
 
-int thirdMax(vector<int>& nums) {
+   /* set<int>::iterator it;
+    for(it = s.begin();it != s.end();it++)
+    {
+        cout << *it <<"\t";
+    }
+    cout << endl;*/
+    return s.size() == 3 ? *s.begin():*s.rbegin();
+}
+int thirdMax_2(vector<int>& nums)
+{
+     if(nums.size() == 1)
+    {
+        return nums[0];   
+    }
+    else if(nums.size() == 2)
+    {
+        return nums[0] > nums[1]?nums[0]:nums[1];   
+    }
+    else
+    {
+        int first = INT_MIN;
+        int sec = INT_MIN;
+        int third = INT_MIN;
+       i// int count = 0;
+        int bottom =0;
+        for(int i = 0 ; i < nums.size();i++)
+        {
+            //说明有个不需要更新
+            if(nums[i] == INT_MIN)
+            {
+                bottom = 1;
+            }
+            //跳过重复的数字
+            if(nums[i] == first || nums[i] == sec || nums[i] == third)
+            {
+                continue;
+            }
+            if(nums[i] > first)
+            {
+                third = sec;
+                sec = first;
+                first = nums[i];
+            }
+            else if(nums[i] > sec)
+            {
+                third = sec;
+                sec = nums[i];
+            }
+            else if(nums[i] >third)
+            {
+                third = nums[i];
+            }
+        }
+        return bottom ? sec:third;
+    } 
+}
+//超时
+int thirdMax_3(vector<int>& nums) {
     if(nums.size() == 1)
     {
         return nums[0];   
@@ -44,8 +115,10 @@ int thirdMax(vector<int>& nums) {
 int main()
 {
     vector<int> data;
-    data.push_back(3);
-    data.push_back(1);
     data.push_back(2);
+    data.push_back(2);
+    data.push_back(3);
+    data.push_back(1)
     cout << thirdMax(data) << endl;
+    cout << thirdMax_2(data) << endl;
 }
