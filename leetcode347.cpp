a48afc9863c7dd36cfe19dddc6066f77376ebead
heng <hengzhang0630@126.com>
2016-10-27 19:54:44 -0700

    Top K Frequent Elements

--- leetcode347.cpp
+++ leetcode347.cpp
@@ -6,30 +6,41 @@
  ************************************************************************/
 
 #include<iostream>
+#include<tr1/unordered_map>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 vector<int> topKFrequent(vector<int>& nums, int k) {
-  /*  if(nums.size() <= 1)
+    std::tr1:: unordered_map<int,int> num_map;
+    for(int i = 0 ; i < nums.size();i++)
     {
-        return nums;
-    }*/
-    vector<int> res;
-    int i = 1 ;
-    int cur = 0;
-    res.push_back(nums[0]);
-    while(i < nums.size())
-    {
-        if(nums[i] != nums[cur])
+        if(num_map.find(nums[i]) != num_map.end())
         {
-            cur++;
-            res.push_back(nums[i]);
-            if(cur + 1 == k)
-            {
-                break;
-            }
+            num_map[nums[i]]++;
         }
+        else
+        {
+            num_map[nums[i]] = 1;
+        }
+    }
+    vector<pair<int,int> > vtMap;
+    for(auto it =num_map.begin();it != num_map.end();it++)
+    {
+        vtMap.push_back(make_pair(it->first,it->second));
+    }
+    sort(vtMap.begin(),vtMap.end(),[](const pair<int,int> &x,const pair<int,int> &y)-> int{ return x.second > y.second; });
+    vector<int> res;
+    int i = 0;
+    for(auto it = vtMap.begin();it!=vtMap.end();it++)
+    {
+       // cout << it->first << ":" <<it->second<<endl;
+       res.push_back(it->first);
         i++;
+        if(i >= k)
+        {
+            break;
+        }
     }
     return res;
 }
