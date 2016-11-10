bf4dd813ab8d4bd6c69c64f990e0a1e3400a153a
heng <hengzhang0630@126.com>
2016-11-09 22:24:16 -0800

    First Unique Character in a String

---
+++ leetcode387.cpp
@@ -0,0 +1,49 @@
+/*************************************************************************
+	> File Name: leetcode387.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Wed 09 Nov 2016 07:15:22 PM PST
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int firstUniqChar(string s) {
+    //下标表示字母 pair 第一个表示字母下标，第二个表示是否有重复
+    vector<pair<int,int> > letter;
+    for(int i = 0; i < 26; i++)
+    {
+        letter.push_back(make_pair(s.length(),-1));
+    }
+    for(int i = 0 ; i < s.length(); i++)
+    {
+        if(letter[s[i] - 'a'].first <s.length())
+        {
+            //标记重复出现的字母
+            letter[s[i] - 'a'].second = 1;
+        }
+        else
+        {
+            //记录第一个出现的字符的下标
+            letter[s[i] - 'a'].first = i;
+        }
+    }
+    int min = s.length();
+    for(int i = 0; i < 26; i++)
+    {
+        cout << (char)('a'+i) <<"==="<< letter[i].first <<"==="<< letter[i].second <<endl;
+        if(letter[i].second == -1 && letter[i].first < min)
+        {
+            min = letter[i].first;
+        }
+    }
+    return min;
+}
+int main()
+{
+    string s = "leetcodecpp";
+    cout << firstUniqChar(s) << endl;
+    string s1= "loveleetcode";
+    cout << firstUniqChar(s1)<< endl;
+}
