11c2fb75dc5e8db1d6b7cc84d148a04840b1073d
heng <hengzhang0630@126.com>
2016-11-09 19:04:34 -0800

    Lexicographical Numbers

--- leetcode386.cpp
+++ leetcode386.cpp
@@ -19,18 +19,47 @@ void dfs(int cur, int n)
             dfs(cur * 10 + i, n);
 
 }
-vector<int> lexicalOrder(int n) {
+vector<int> lexicalOrder_2(int n) {
     for(int i=1; i<=9; i++)
         dfs(i, n);
     return ans;
 }
-
+//方法二
+vector<int> lexicalOrder(int n) {
+    vector<int> result(n);
+    int index = 1;
+    for(int i = 0; i < n; i++)
+    {
+        result[i] = index;
+        if(index * 10 <= n)
+        {
+            index *= 10;
+        }
+        else
+        {
+            if(index >= n)
+            {
+                index /= 10;
+            }
+            index++;
+            while(0 == index % 10)
+            {
+                index /= 10;
+            }
+        }
+    }
+    return result;
+}
 int main()
 {
-    vector<int> res = lexicalOrder(105);
+    vector<int> res = lexicalOrder(115);
     for(int i = 0 ; i < res.size();i++)
     {
         cout << res[i] <<'\t';
+        if(i % 10 == 0)
+        {
+            cout << endl;
+        }
     }
     cout <<endl;
 
