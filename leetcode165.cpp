64178b741f016c9e98cb49d18b5b05e44593c214
heng <hengzhang0630@126.com>
2016-11-22 22:40:50 -0800

    Compare Version Numbers

--- leetcode165.cpp
+++ leetcode165.cpp
@@ -6,56 +6,86 @@
  ************************************************************************/
 
 #include<iostream>
+#include<stdlib.h>
 #include<string>
 using namespace std;
 
 int compareVersion(string version1, string version2) {
     int pos1 = version1.find('.',0);
     int pos2 = version2.find('.',0);
-    cout << pos1 <<"-----"<< pos2<<endl;
-    if(pos1 > pos2)
+    cout << version1.substr(0,pos1) <<"***" << version1.substr(pos1 + 1,version1.length() - pos1) << endl;
+
+    cout << version2.substr(0,pos2) <<"***" << version2.substr(pos2 + 1,version2.length() - pos2) << endl;
+    if(pos1 == -1 && pos2 == -1)
     {
-        return 1;
-    }
-    else if(pos1 < pos2)
-    {
-        return -1;
-    }
-    else
-    {
-        for(int i = 0 ;i < pos1; i++)
-        {
-            if(version1[i] > version2[i])
-            {
-                return 1;
-            }
-            else if(version1[i] < version2[i])
-            {
-                return -1;
-            }
-        }
-        int leftPos1 = version1.length() - pos1;
-        int leftPos2 = version2.length() - pos2;
-        if(leftPos1 > leftPos2)
+        int left1 = atoi(version1.c_str());
+        int left2 = atoi(version2.c_str());
+        if(left1 > left2)
         {
             return 1;
         }
-        else if(leftPos1 < leftPos2)
+        else if(left1 < left2)
         {
             return -1;
         }
-        int len = leftPos1 > leftPos2 ? version2.length(): version1.length() ;
-        for(int i = pos1+1;i < len ;i++)
+        else
         {
-            if(version1[i] > version2[i])
+            return 0;
+        }
+    }
+    else if(pos1 != -1 && pos2 != -1)
+    {
+        int left1 = atoi(version1.substr(0,pos1).c_str());
+        int left2 = atoi(version2.substr(0,pos2).c_str());
+        if(left1 > left2)
+        {
+            return 1;
+        }
+        else if(left1 < left2)
+        {
+            return -1;
+        }
+        else
+        {
+            int right1 = atoi(version1.substr(pos1 + 1, version1.length() - pos1).c_str());
+            int right2 = atoi(version2.substr(pos2 + 1, version2.length() - pos1).c_str());
+            if(right1 > right2)
             {
                 return 1;
             }
-            else if(version1[i] < version2[i])
+            else if(right1 < right2)
             {
                 return -1;
             }
+            else
+            {
+                return 0;
+            }
         }
+    }
+    else
+    {
+        int left1 = 0;
+        int left2 = 0;
+        if(pos1 == -1)
+        {
+            left1 = atoi(version1.c_str());
+            left2 = atoi(version2.substr(0,pos2).c_str());
+        }
+        else
+        {
+            left2 = atoi(version2.c_str());
+            left1 = atoi(version1.substr(0,pos1).c_str());
+        
+        } 
+        if(left1 > left2)
+            {
+                return 1;
+            }
+            else if(left1 < left2)
+            {
+                return -1;
+            }
     }
     return 0;
 }
@@ -66,4 +96,6 @@ int main()
     cout <<"(1.2,1.21),***"<<compareVersion("1.2","1.21") << endl;
     cout <<"(1.2,1.12),***"<<compareVersion("1.2","1.12") << endl;
     cout <<"(1.2,1.1),***"<<compareVersion("1.2","1.1") << endl;
+    cout <<"(1,1),***"<<compareVersion("1","1") << endl;
+    cout <<"(01,1),***"<<compareVersion("01","1") << endl;
 }
