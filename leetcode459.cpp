367cadc2e85cd0a7480f5919267ae0fe01104794
heng <hengzhang0630@126.com>
2016-12-07 18:53:34 -0800

    add another method to solve the problem of  Repeated Substring Pattern

--- leetcode459.cpp
+++ leetcode459.cpp
@@ -20,7 +20,7 @@ bool IsSame(string str, int i)
     return true;
 } 
 
-bool repeatedSubstringPattern(string str) {
+bool repeatedSubstringPattern_2(string str) {
     if(str.size() < 2)
     {
         return false;
@@ -31,6 +31,31 @@ bool repeatedSubstringPattern(string str) {
         if(IsSame(str,i))
         {
             return true;
+        }
+    }
+    return false;
+}
+//方法二
+bool repeatedSubstringPattern(string str) {
+    int len = str.size();
+    if(len < 2)
+    {
+        return false;
+    }
+    for(int i = len / 2; i > 0; i--)
+    {
+        if(len % 2 == 0)
+        {
+            string tmp = "";
+            string sub = str.substr(0,i);
+            for(int j = 0 ; j < len / i; j++)
+            {
+                tmp += sub;
+            }
+            if(str == tmp)
+            {
+                return true;
+            }
         }
     }
     return false;
