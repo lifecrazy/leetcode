aa23ece6dc29b4d4378776444d47e6b700152865
heng <hengzhang0630@126.com>
2016-12-12 18:35:18 -0800

    Letter Combinations of a Phone Number

--- leetcode017.cpp
+++ leetcode017.cpp
@@ -12,6 +12,10 @@
 using namespace std;
 vector<string> letterCombinations(string digits) {
     vector<string> res;
+    if(digits.size() <= 0)
+    {
+        return res;
+    }
     string charmap[10] = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     res.push_back("");
     for (int i = 0; i < digits.size(); i++)
