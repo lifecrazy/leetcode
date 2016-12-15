5804fa60241a2eb14a0d2056eefda042a7752f49
heng <hengzhang0630@126.com>
2016-12-15 00:59:41 -0800

    Valid Number

--- leetcode065.cpp
+++ leetcode065.cpp
@@ -22,14 +22,14 @@ bool isNumber(string s) {
         right--;
     }
     cout << "left:"<<left<<"right:"<<right<<endl;
-    if((left >= right)&&(s[left] <'0' || s[left] >'9'))
-    {
-        return false;
-    }
     //如果第一个非空字符为正负号则跳过
     if(s[left] == '-'||s[left] == '+')
     {
         left++;
+    }
+    if((left >= right)&&(s[left] <'0' || s[left] >'9'))
+    {
+        return false;
     }
     //如果第一个字符或者最后一个字符为e或者E肯定不是数字
     if(s[left] == 'e'||s[left] == 'E'||s[right] == 'e'||s[right] == 'E')
@@ -37,6 +37,8 @@ bool isNumber(string s) {
         return false;
     }
     int pointCount = 0 ;
+    int ePos = -1;
+    int eCount = 0;
     for(int i = left; i <= right; i++)
     {
         //字符串中含有0-9eE不处理
@@ -46,21 +48,31 @@ bool isNumber(string s) {
         }
         else if(s[i] == 'e'||s[i] == 'E')
         {
+            ePos = i;
             if(i+1 <right && (s[i+1]=='-'||s[i+1]=='+'))
             {
                 i++;
             }
+            eCount++;
         }
         //.可以处在字符串的任何位置但只能出现一次
         else if(s[i] == '.')
         {
+            if(ePos != -1 && ePos < i)
+            {
+                return false;
+            }
+            if((i+1 < right &&i == left&& (s[i+1] == 'e'||s[i+1] == 'E'))||(i-1>left && i == right&&(s[i-1] == 'e'||s[i-1] == 'E')))
+            {
+                return false;                
+            }
             pointCount++;
         }
         else
         {
             return false;
         }
-        if(pointCount > 1)
+        if(pointCount > 1 || eCount > 1)
         {
             return false;
         }
@@ -77,4 +89,7 @@ int main()
     cout <<"2e10:" << isNumber("2e10") <<endl;
     cout <<"..:" << isNumber("..") <<endl;
     cout <<"+1.e+5:" << isNumber("+1.e+5") <<endl;
+    cout <<".e1:" << isNumber(".e1") <<endl;
+    cout <<"3ee5:" << isNumber("3ee5") <<endl;
+    cout <<"3e.5:" << isNumber("3e.5") <<endl;
 }
