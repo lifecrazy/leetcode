9a1a1a73d254f29fbc9f79364157f6791c61df54
lifecrazy <hengzhang0630@126.com>
2024-01-10 15:37:38 +0800

    Update leetcode009.cpp
    bug fix for largest number

--- leetcode009.cpp
+++ leetcode009.cpp
@@ -9,16 +9,19 @@
 using namespace std;
 
 bool isPalindrome(int x) {
-    if(x < 0)
-        return false;
-    int i = x;
-    int j = 0;
-    while(i != 0)
-    {
-        j = j * 10 + (i % 10);
-        i = i / 10;
-    }
-    return x == j;
+        if (x < 0)
+            return false;
+        int i = x;
+        int j = 0;
+        while (i != 0) {
+                if (j > INT_MAX / 10 || (j == INT_MAX / 10 && i > INT_MAX % 10)) {
+                    return false;
+                }
+            j = j * 10 + (i % 10);
+
+            i = i / 10;
+        }
+        return x == j;
 }
 
 int main()
